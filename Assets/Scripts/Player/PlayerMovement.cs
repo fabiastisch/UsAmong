35876bb67614d0e0525d17679e435c1a5e13271b
Fabian Weber <faweber@stud.hs-heilbronn.de>
2021-05-20 17:06:34 +0200

    add minimalistic playermovement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using MLAPI;
+using UnityEngine;
+
+namespace Player {
+    [RequireComponent(typeof(NetworkObject), typeof(Rigidbody2D))]
+    public class PlayerMovement : NetworkBehaviour {
+        public float speed = 10;
+
+
+        private Rigidbody2D rb;
+    
+        // Start is called before the first frame update
+        void Start() {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (!IsLocalPlayer) {
+                return;
+            }
+            float horizontal = Input.GetAxis("Horizontal"); // d = 1, a = -1
+            float vertical = Input.GetAxis("Vertical"); // w = 1 , s = -1 
+
+            Vector2 velocity = new Vector2(horizontal, vertical);
+            rb.velocity = velocity * speed;
+        }
+    }
+}
