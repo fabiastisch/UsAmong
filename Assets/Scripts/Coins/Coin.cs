d12dc2b9923764477fe617c755af8c5a47b920b0
Fabian Weber <faweber@stud.hs-heilbronn.de>
2021-06-22 16:51:40 +0200

    update coin

--- Assets/Scripts/Coins/Coin.cs
+++ Assets/Scripts/Coins/Coin.cs
@@ -1,30 +1,14 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Coin : MonoBehaviour {
-    private bool isAvailabe = true;
-    // Start is called before the first frame update
-    void Start() {
-        Debug.Log("Created");
-    }
-
-    // Update is called once per frame
-    void Update() {
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Destroy Coin"); // TODO: called twice
-        CoinManager.Instance.minimizeRemainingCoins(gameObject);
-    }
-
-    private void OnDestroy() {
-        /*
-        if (isAvailabe) {
-            
-            isAvailabe = false;
+namespace Coins {
+    public class Coin : MonoBehaviour {
+        void Start() {
+            //Debug.Log("[Coin] Created");
         }
-    */
+    
+        private void OnTriggerEnter2D(Collider2D other) {
+            CoinManager.Instance.minimizeRemainingCoins(gameObject);
+        }
+    
     }
 }
