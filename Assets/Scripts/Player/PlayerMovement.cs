1fa5f63b39987f695bbc5e74721112daea68e14c
Fabian Weber <faweber@stud.hs-heilbronn.de>
2021-06-25 09:52:40 +0200

    Add Player animation / looking left or right

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,13 +5,17 @@ namespace Player {
     [RequireComponent(typeof(NetworkObject), typeof(Rigidbody2D))]
     public class PlayerMovement : NetworkBehaviour {
         public float speed = 10;
-
-
+        
         private Rigidbody2D rb;
-    
+
+        private Animator anim;
+        private int leftHash = Animator.StringToHash("Left");
+        private int rightHash = Animator.StringToHash("Right");
+
         // Start is called before the first frame update
         void Start() {
             rb = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
         }
 
         // Update is called once per frame
@@ -24,6 +28,14 @@ namespace Player {
 
             Vector2 velocity = new Vector2(horizontal, vertical);
             rb.velocity = velocity * speed;
+
+            if (horizontal > 0) { // show right
+                anim.SetTrigger(rightHash);
+            }
+
+            if (horizontal < 0) { // show left
+                anim.SetTrigger(leftHash);
+            }
         }
     }
 }
